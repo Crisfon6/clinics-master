4a53056ce0517167f047c54a780a7ff0479aed64
iepenaranda <iepo960709@gmail.com>
2021-05-28 23:15:16 -0500

    Agregado server.js

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3001;
+    this.connectDB();
+  }
+
+  connectDB() {
+    mongoose
+      .connect("mongodb://localhost:27017/equiposMedicos", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      })
+      .then(() => console.log("Conexión con MongoDB: ON"))
+      .catch((error) => console.log("Error al conectar con MongoDB: " + error));
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use("/api/user");
+  }
+
+  listen() {
+    this.app.listen(this.port, () =>
+      console.log("Servidor ejecuta en puerto: " + this.port)
+    );
+  }
+}
+
+const server = new Server();
+server.listen();
