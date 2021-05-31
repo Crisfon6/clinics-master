93d38bc1654b2bb89d2fc356b39702228d528384
iepenaranda <iepo960709@gmail.com>
2021-05-31 15:58:00 -0500

    Roles y History v1.1

--- backend/models/history.js
+++ backend/models/history.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const historySchema = new mongoose.Schema({
-    description: String,
-    date: {type: Date,
-        default: Date.now},
-        inventary: String,
-        executeBy: String,
-}) 
 
-const History = mongoose.model("Historial", historySchema)
-module.exports = History
+const historySchema = new mongoose.Schema({
+  description: String,
+  executeBy: String,
+  inventary: String,
+  date: { type: Date, default: Date.now },
+});
+
+const History = mongoose.model("History", historySchema);
+module.exports = History;
--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,28 +1,33 @@
 const express = require("express");
-const Rol = require("../models/role");
+const Role = require("../models/role");
 const router = express.Router();
 const Auth = require("../middleware/auth");
+const User = require("../models/user");
 
-router.post("/RegistrarRol", async (req, res) => {
-  const rol = await Rol.findOne({ name: req.body.name });
-  if (rol) return res.status(400).send("El rol ya existe");
-  const role = await req.body;
-  console.log(role);
-  const newRol = new Rol({
+router.post("/registerRol", async (req, res) => {
+  const role = await Role.findOne({ name: req.body.name });
+  if (role) return res.status(400).send("El rol ya existe");
+
+  const newRole = new Rol({
     name: req.body.name,
     description: req.body.description,
     active: req.body.active,
   });
-  const result = await newRol.save();
+
+  const result = await role.save();
   return res.status(200).send({ result });
 });
 
-router.get("/consultarroles", Auth, async (req, res) => {
-  const rol = await Rol.find({ name: req.body.name });
-  return res.status(200).send({ rol });
+router.get("/getRoles", Auth, async (req, res) => {
+  const role = await Role.find();
+  if (!role)
+    return res
+      .status(400)
+      .send("No se encontraron roles registrados en la BD.");
+  return res.status(200).send({ role });
 });
 
-router.put("/EditarRol", Auth, async (req, res) => {
+router.put("/updateRole", Auth, async (req, res) => {
   const rol = await Rol.findByIdAndUpdate(req.body._id, {
     name: req.body.name,
     description: req.body.description,
@@ -30,13 +35,23 @@ router.put("/EditarRol", Auth, async (req, res) => {
   });
 
   if (!rol) return res.status(400).send("El rol no se pudo actualizar");
-
   return res.status(200).send({ rol });
 });
 
 router.delete("/:_id", Auth, async (req, res) => {
-  const rol = await Rol.findByIdAndDelete(req.params._id);
-  if (!rol) return res.status(400).send("No se pudo eliminar el rol");
+  const user = await User.findbyId(req.user._id);
+  if (!user)
+    return res
+      .status(400)
+      .send("Usuario no autenticado, por favor inicie sesion.");
+
+  const rol = await Rol.findByIdAndUpdate(req.params._id, {
+    name: rol.name,
+    description: rol.description,
+    active: false,
+  });
+
+  if (!rol) return res.status(400).send("No se pudo eliminar el rol.");
   return res.status(200).send("Rol eliminado");
 });
 
