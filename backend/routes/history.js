21d04268b282e0e0474483fbd35e131b7dbf50bb
iepenaranda <iepo960709@gmail.com>
2021-05-31 11:22:13 -0500

    Historial

---
+++ backend/routes/history.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const Historial = require("../models/historial");
+const User = require("../models/user")
+const Inventary = require("../models/inventary")
+const router = express.Router()
+const Auth = require("../middleware/auth");
+
+router.post("/registrarhistorial", Auth, async (req, res) => {
+    const user = await User.findById(req.user._id)
+    const inventary = await Inventary.findById(req.inventary._id)
+    const historial = new Historial({
+        descrition: req.body.descrition,
+        inventary: inventary._id,
+        executeBy: user._id,
+
+    })
+    const result = await historial.save()
+    return res.status(200).send({result})
+})
