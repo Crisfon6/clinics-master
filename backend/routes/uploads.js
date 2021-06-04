c5439a05f30ebf0cac1ac4780a64075ae4670587
cristhian <disruptivecrisfon6@gmail.com>
2021-06-04 15:48:05 -0500

    before add image-type package

--- backend/routes/uploads.js
+++ backend/routes/uploads.js
@@ -4,14 +4,16 @@ const multipart = require('connect-multiparty');
 const multipartMiddleware = multipart();
 
 router.post('/:type/:id', multipartMiddleware, async(req, res) => {
-
+    const key = Object.keys(req.files)[0];
+    const file = req.files[key];
+    console.log('FFILE', file);
+    const validTypes = ['CV', 'avatar', 'equipment'];
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send({ msg: 'File no found' });
     }
-    console.log(req.files);
+    console.log(req.files.keys);
     const { type, id } = req.params;
 
-    const validTypes = ['CV', 'avatar', 'equipment'];
     //valid types
     if (!validTypes.includes(type)) {
         res.status(401).status({ msg: 'Invalid Type' });
