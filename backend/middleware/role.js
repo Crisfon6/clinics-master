ef6264b641d503cb166d285ced1dc9e95e8bf721
andvar95 <varleo.95@gmail.com>
2021-06-04 19:52:36 -0500

    multiple rols

--- backend/middleware/role.js
+++ backend/middleware/role.js
@@ -1,27 +1,23 @@
 const Role = require("../models/role");
-
-//  haveRole('ADMIN_ROLE', 'employ'), comnprobar con esto
-
-
 const haveRole = (...role) => {
-    return async(req, res, next) => {
-        let error= true;
-        
-        try{
-        const roleDB = await Role.findById(req.user.role);
-        role.forEach(element=>{
-            if(element === roleDB.name) {
-                error=false
-            }
-        })
-        if(error === true)return res.status(400).send("You need be "+role)
-        next();
-    }
-    catch(eror){
-        res.status(400).send("Error checking role")
-    }
+    return async (req, res, next) => {
+        let error = true;
+        try {
+            const roleDB = await Role.findById(req.user.role);
+            role.forEach(element => {
+                if (element === roleDB.name) {
+                    error = false;
+                }
+            });
+            if (error === true)
+                return res.status(400).send("You need be " + role);
+            next();
+        }
+        catch (eror) {
+            res.status(400).send("Error checking role");
+        }
     };
-};
+}
 module.exports = {
     haveRole,
 };
