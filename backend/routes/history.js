fba4254b01253fafae1a4b5d1c6d79cd8bdd04a6
cristhian <disruptivecrisfon6@gmail.com>
2021-06-05 16:48:21 -0500

    time of fix

--- backend/routes/history.js
+++ backend/routes/history.js
@@ -10,97 +10,97 @@ const haveRole = require("../middleware/role");
 const mongoose = require("mongoose");
 
 router.post(
-  "/newHistory",
-  Auth,
-  userDB,
-  haveRole("admin", "user"),
-  validateData,
-  async (req, res) => {
-    let validId = mongoose.Types.ObjectId.isValid(req.body.equipment);
-    if (!validId) return res.status(400).send("Error: Invalid equipment id.");
-    const equipment = await Equipment.findById(req.body.equipment);
-    if (!equipment) return res.status(400).send("Error: Equipment not found.");
+    "/newHistory",
+    Auth,
+    userDB,
+    haveRole(["admin", "user"]),
+    validateData,
+    async(req, res) => {
+        let validId = mongoose.Types.ObjectId.isValid(req.body.equipment);
+        if (!validId) return res.status(400).send("Error: Invalid equipment id.");
+        const equipment = await Equipment.findById(req.body.equipment);
+        if (!equipment) return res.status(400).send("Error: Equipment not found.");
 
-    validId = mongoose.Types.ObjectId.isValid(req.body.executeBy);
-    if (!validId) return res.status(400).send("Error: Invalid user id.");
-    const user = await User.findById(req.body.executeBy);
-    if (!user) return res.status(400).send("Error: User not found.");
+        validId = mongoose.Types.ObjectId.isValid(req.body.executeBy);
+        if (!validId) return res.status(400).send("Error: Invalid user id.");
+        const user = await User.findById(req.body.executeBy);
+        if (!user) return res.status(400).send("Error: User not found.");
 
-    const history = new History({
-      description: req.body.description,
-      equipment: req.body.equipment,
-      executeBy: req.body.executeBy,
-    });
-    const result = await history.save();
-    if (!result) return res.status(401).send("Error: History was not created.");
-    return res.status(200).send({ result });
-  }
+        const history = new History({
+            description: req.body.description,
+            equipment: req.body.equipment,
+            executeBy: req.body.executeBy,
+        });
+        const result = await history.save();
+        if (!result) return res.status(401).send("Error: History was not created.");
+        return res.status(200).send({ result });
+    }
 );
 
 router.get(
-  "/getHistory/:id",
-  Auth,
-  userDB,
-  haveRole("admin", "user"),
-  validateData,
-  async (req, res) => {
-    const validId = mongoose.Types.ObjectId.isValid(req.params.id);
-    if (!validId) return res.status(400).send("Error: Invalid id.");
+    "/getHistory/:id",
+    Auth,
+    userDB,
+    haveRole("admin", "user"),
+    validateData,
+    async(req, res) => {
+        const validId = mongoose.Types.ObjectId.isValid(req.params.id);
+        if (!validId) return res.status(400).send("Error: Invalid id.");
 
-    let history = await History.findById(req.params.id)
-      .populate([
-        { path: "equipment", select: "name" },
-        { path: "executeBy", select: "name -_id" },
-      ])
-      .exec();
+        let history = await History.findById(req.params.id)
+            .populate([
+                { path: "equipment", select: "name" },
+                { path: "executeBy", select: "name -_id" },
+            ])
+            .exec();
 
-    if (!history) return res.status(401).send("No history were found.");
-    return res.status(200).send({ history });
-  }
+        if (!history) return res.status(401).send("No history were found.");
+        return res.status(200).send({ history });
+    }
 );
 
 router.put(
-  "/editHistory",
-  Auth,
-  userDB,
-  haveRole("admin"),
-  validateData,
-  async (req, res) => {
-    const validId = mongoose.Types.ObjectId.isValid(req.body._id);
-    if (!validId) return res.status(400).send("Error: Invalid id.");
+    "/editHistory",
+    Auth,
+    userDB,
+    haveRole("admin"),
+    validateData,
+    async(req, res) => {
+        const validId = mongoose.Types.ObjectId.isValid(req.body._id);
+        if (!validId) return res.status(400).send("Error: Invalid id.");
 
-    const history = await History.findByIdAndUpdate(req.body._id, {
-      description: req.body.description,
-      equipment: req.body.equipment,
-      executeBy: req.body.executeBy,
-      status: req.body.status,
-    });
+        const history = await History.findByIdAndUpdate(req.body._id, {
+            description: req.body.description,
+            equipment: req.body.equipment,
+            executeBy: req.body.executeBy,
+            status: req.body.status,
+        });
 
-    if (!history) return res.status(401).send("No history were found.");
-    return res.status(200).send({ history });
-  }
+        if (!history) return res.status(401).send("No history were found.");
+        return res.status(200).send({ history });
+    }
 );
 
 router.put(
-  "/deleteHistory",
-  Auth,
-  userDB,
-  haveRole("admin"),
-  validateData,
-  async (req, res) => {
-    const validId = mongoose.Types.ObjectId.isValid(req.body._id);
-    if (!validId) return res.status(400).send("Error: Invalid id.");
+    "/deleteHistory",
+    Auth,
+    userDB,
+    haveRole("admin"),
+    validateData,
+    async(req, res) => {
+        const validId = mongoose.Types.ObjectId.isValid(req.body._id);
+        if (!validId) return res.status(400).send("Error: Invalid id.");
 
-    const history = await History.findByIdAndUpdate(req.body._id, {
-      description: req.body.description,
-      equipment: req.body.equipment,
-      executeBy: req.body.executeBy,
-      status: false,
-    });
+        const history = await History.findByIdAndUpdate(req.body._id, {
+            description: req.body.description,
+            equipment: req.body.equipment,
+            executeBy: req.body.executeBy,
+            status: false,
+        });
 
-    if (!history) return res.status(401).send("No history were deleted.");
-    return res.status(200).send("The history was deleted");
-  }
+        if (!history) return res.status(401).send("No history were deleted.");
+        return res.status(200).send("The history was deleted");
+    }
 );
 
 module.exports = router;
