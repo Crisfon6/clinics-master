3294acded6de5e03816dcf4170c6f84471bf3a27
smercadog14 <smercadog14@gmail.com>
2021-06-06 01:19:44 -0500

    Add Joi as Dependency

--- backend/middleware/validateData.js
+++ backend/middleware/validateData.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const valedateBody = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
-    console.log(error);
+
     if (error) {
       const errorMessage = error.details.map((error) => error.message);
       return res.status(401).json({ message: errorMessage });
